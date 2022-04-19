3eb913c3cfab5098251ce1e941f52301e23b0d2f
musfikuroli <musfikuroli@gmail.com>
2022-04-19 19:52:28 +0600

    Initial Commit

---
+++ app/src/main/java/com/musfikuroli/bda/Email/Util.java
@@ -0,0 +1,7 @@
+package com.musfikuroli.bda.Email;
+
+public class Util {
+    // Add your own email and password over here.
+    public  static  final String EMAIL = "therewillbebloodmobileapp@gmail.com";
+    public static final String PASSWORD = "therewillbeblood@12345";
+}
